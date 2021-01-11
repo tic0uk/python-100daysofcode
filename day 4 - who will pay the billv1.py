0b08557bf4a39d5311ad0afafede40382631a8aa
Tim Craig <64689948+tic0uk@users.noreply.github.com>
2021-01-11 11:14:24 +0000

    Update day 4 - who will pay the billv1.py

--- day 4 - who will pay the billv1.py
+++ day 4 - who will pay the billv1.py
@@ -4,9 +4,6 @@
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
-# 🚨 Don't change the code above 👆
-#Write your code below this line 👇
-
 #import the random module
 import random
 
