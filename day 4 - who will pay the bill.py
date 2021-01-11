cd41572b1c7b31558ecae6ffc715935eb2714e2f
Tim Craig <64689948+tic0uk@users.noreply.github.com>
2021-01-11 11:11:56 +0000

    Create day 4 - who will pay the bill.py

---
+++ day 4 - who will pay the bill.py
@@ -0,0 +1,23 @@
+# random generator to work out who will pay the bill
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+
+#import the random module
+import random
+
+# count the number of items in the list
+num_list_items = len(names)
+
+# generate a random number between 0 and count of items (take off one for 0 offset)
+random_number = random.randint(0,num_list_items - 1)
+
+# choose the name
+random_name = names[random_number] 
+
+# deliver the result
+print(f"{random_name} is going to buy the meal today!")
