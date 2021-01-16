03ca8900ca7a72a4e11bd89300606c20bb4d5f45
Tim Craig <64689948+tic0uk@users.noreply.github.com>
2021-01-16 15:38:09 +0000

    Create day 9 - blind auction program.py

---
+++ day 9 - blind auction program.py
@@ -0,0 +1,38 @@
+# blind auction program that allows users to enter name and bid without seeing others bids 
+# then works out highest bid. 
+# uses a dictionary within a list
+
+from replit import clear # uses repl.it module
+from art import logo # uses separate file not included
+print(logo)
+print("Welcome to the secret auction program.")
+auction = []
+end = False
+def add_new_bid(name_input, bid_input):        
+        auction.append({
+            "name" : name_input,
+            "bid"  : bid_input
+        })
+ 
+while not end:
+        name = input("What is your name?: ")
+        bid = input("What is your bid?: $")
+        other_bidders = input("Are there any other bidders? Type 'yes', or 'no'.\n").lower()
+        
+        add_new_bid(name_input=name, bid_input=bid)
+        if other_bidders == "yes":
+            end = False
+            clear()
+        elif other_bidders == "no":
+            end = True
+top_bid = 0
+for item in auction:
+    position = auction.index(item)
+    bid_amount = int(auction[position]["bid"])
+    bid_user = auction[position]["name"]
+    if bid_amount > top_bid:
+        top_bid = bid_amount
+        top_bidder = bid_user
+clear()
+name="Tim"
+print(f"The winner is {top_bidder} with a bid of ${top_bid}")
